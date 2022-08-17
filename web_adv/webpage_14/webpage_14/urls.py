6d30ae5f7ddea03693177287a6c0ffe92fc8944d
Karthikeya Enge <karthikeya.enge@hotmail.com>
2022-08-17 23:10:28 +0530

    added

--- web_adv/webpage_14/webpage_14/urls.py
+++ web_adv/webpage_14/webpage_14/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('page1/', include("page1.urls"))
 ]
