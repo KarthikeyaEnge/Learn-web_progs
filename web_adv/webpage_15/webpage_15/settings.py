c0ea29435071a2c9e7cce30511298287a310b3db
Karthikeya Enge <karthikeya.enge@hotmail.com>
2022-08-29 07:59:28 +0530

    removed issues

--- web_adv/webpage_15/webpage_15/settings.py
+++ web_adv/webpage_15/webpage_15/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3lm#c!b%_x7q@1ubvbeo@23=u3ho9p7$9g1g6n9k!9j+a5mrq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
