b940d149a392bf8e0993936963e0c77175af2c49
Karthikeya Enge <karthikeya.enge@hotmail.com>
2022-08-31 16:50:56 +0530

    updated

--- web_adv/webpage_14/webpage_14/settings.py
+++ web_adv/webpage_14/webpage_14/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#mrk-$6yvjhxzs0shjxghr4hw%_v2tfj5!bg&r)%r-h3=w23$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
